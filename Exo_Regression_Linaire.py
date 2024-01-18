883c1375f2eb9fa76d74f85cb0ebda11b8da4752
Jerome Reviron <jrinstitut63@hotmail.fr>
2024-01-18 15:14:18 +0100

    Mise à jour avec la Moyenne ln(N)

--- Exo_Regression_Linaire.py
+++ Exo_Regression_Linaire.py
@@ -19,3 +19,15 @@ moyenne_t_carre_moins_7_carre = ((sum(x**2 for x in t) / len(t)) - 7**2)
 
 # Affichage du résultat
 print(f"La moyenne des carrés des éléments de la série t moins le carré de 7 est : {moyenne_t_carre_moins_7_carre}")
+
+
+
+### Moyenne de ln(N)
+# Série de nombres
+t = [0.69, 1.61, 2.77, 2.99, 3.69, 4.60, 5.30, 5.77]
+
+# Calcul de la moyenne
+moyenne_t = sum(t) / len(t)
+
+# Affichage du résultat
+print(f"La moyenne de la série t est : {round(moyenne_t, 2)}")
