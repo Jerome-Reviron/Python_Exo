5cbd9652d78f2ea44cb28197c6756a14804948e0
Jerome Reviron <jrinstitut63@hotmail.fr>
2024-01-18 15:36:12 +0100

    Correction erreur Bleu Pylint

--- Exo_Regression_Linaire.py
+++ Exo_Regression_Linaire.py
@@ -15,10 +15,10 @@ print(f"La moyenne de la série t est :{moyenne_t}")
 t = [0, 2, 4, 6, 8, 10, 12, 14]
 
 # Calcul de la moyenne des carrés des éléments de la série t moins le carré de 7.
-moyenne_t_carre_moins_7_carre = ((sum(x**2 for x in t) / len(t)) - 7**2)
+moyenne_t_carre_moins_7_carre = (sum(x**2 for x in t) / len(t)) - 7**2
 
 # Affichage du résultat
-print(f"La moyenne des carrés des éléments de la série t moins le carré de 7 est : {moyenne_t_carre_moins_7_carre}")
+print(f"La moyenne des carrés des éléments t - le carré de 7 est :{moyenne_t_carre_moins_7_carre}")
 
 
 
